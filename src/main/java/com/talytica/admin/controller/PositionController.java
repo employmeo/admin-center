389df223f46466e56f7ad633933d9d7e7229ac22
Sri <sridharkaza@yahoo.com>
2017-05-22 08:51:45 -0700

    added ability to edit / mange more items

---
+++ src/main/java/com/talytica/admin/controller/PositionController.java
@@ -0,0 +1,89 @@
+package com.talytica.admin.controller;
+
+import java.lang.reflect.Field;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.employmeo.data.model.Account;
+import com.employmeo.data.model.Criterion;
+import com.employmeo.data.model.Position;
+import com.employmeo.data.model.PredictionModel;
+import com.employmeo.data.repository.CriterionRepository;
+import com.employmeo.data.service.AccountService;
+import com.employmeo.data.service.PredictionModelService;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin/position")
+public class PositionController {
+
+	@Autowired
+	AccountService accountService;
+
+	private static final String FRAGMENT_ROOT = "model/";
+	private static final String MODEL = "position";
+	private static final String MODEL_DISPLAY = "Position";
+	private static final String LIST_VIEW = FRAGMENT_ROOT + MODEL + "/list";
+	//private static final String CREATE_VIEW = FRAGMENT_ROOT + MODEL + "/create";
+	private static final String EDIT_VIEW = FRAGMENT_ROOT + MODEL + "/edit";
+	private static final String DISPLAY_VIEW = FRAGMENT_ROOT + MODEL + "/view";
+	private static final Class MODEL_CLASS= Position.class;
+
+    @RequestMapping(value = {"","/list"}, method = RequestMethod.GET)
+    public String  list(Model model) {
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+    	model.addAttribute("items", accountService.getAllPositions());
+    	return LIST_VIEW;
+    }
+   
+    @RequestMapping(value = "create", method = RequestMethod.GET)
+    public String create(Model model){
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+    	model.addAttribute("item", new Criterion());
+    	return EDIT_VIEW;
+    }
+    
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(Position position, Model model) {
+    	Position saved = accountService.save(position);
+        return "redirect:/admin/" + MODEL + "/" + saved.getId();
+    }
+
+    @RequestMapping(value = "edit/{id}", method = RequestMethod.GET)
+    public String edit(@PathVariable Long id, Model model){
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+        model.addAttribute("item", accountService.getPositionById(id));
+        return EDIT_VIEW;
+    }
+    
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.GET)
+    public String delete(@PathVariable Long id, Model model){
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+    	accountService.delete(accountService.getPositionById(id));
+    	model.addAttribute("items", accountService.getAllPositions());
+        return LIST_VIEW;
+    }
+    
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public String view(@PathVariable Long id, Model model){
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+        model.addAttribute("item", accountService.getPositionById(id));
+        return DISPLAY_VIEW;
+    }
+     
+    @ModelAttribute("fieldnames")
+    public Field[] getFieldNames() {  	
+        return MODEL_CLASS.getDeclaredFields();
+    }
+}
