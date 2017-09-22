e4f8c4df2bbb322c69237e45fe094ee6f31f904b
Sri <sridharkaza@yahoo.com>
2017-09-22 11:42:13 -0700

    adding eval config and workflow config

---
+++ src/main/java/com/talytica/admin/controller/EvalConfigController.java
@@ -0,0 +1,81 @@
+package com.talytica.admin.controller;
+
+import java.lang.reflect.Field;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.employmeo.data.model.GraderConfig;
+import com.employmeo.data.service.AccountSurveyService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin/evalconfig")
+public class EvalConfigController {
+
+	@Autowired
+	AccountSurveyService accountSurveyService;
+
+	private static final String FRAGMENT_ROOT = "model/";
+	private static final String MODEL = "evalconfig";
+	private static final String MODEL_DISPLAY = "Account Survey Evaluation Config";
+	private static final String LIST_VIEW = FRAGMENT_ROOT + MODEL + "/list";
+	//private static final String CREATE_VIEW = FRAGMENT_ROOT + MODEL + "/create";
+	private static final String EDIT_VIEW = FRAGMENT_ROOT + MODEL + "/edit";
+	private static final String DISPLAY_VIEW = FRAGMENT_ROOT + MODEL + "/view";
+	private static final Class MODEL_CLASS= GraderConfig.class;
+
+    @RequestMapping(value = {"","/list"}, method = RequestMethod.GET)
+    public String  list(Model model) {
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+    	model.addAttribute("items", accountSurveyService.getAllGraderConfigs());
+    	return LIST_VIEW;
+    }
+   
+    @RequestMapping(value = "create", method = RequestMethod.GET)
+    public String create(Model model){
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+    	model.addAttribute("item", new GraderConfig());
+    	return EDIT_VIEW;
+    }
+    
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(GraderConfig config, Model model) {
+    	GraderConfig saved = accountSurveyService.save(config);
+        return "redirect:/admin/" + MODEL + "/" + saved.getId();
+    }
+
+    @RequestMapping(value = "edit/{id}", method = RequestMethod.GET)
+    public String edit(@PathVariable Long id, Model model){
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+        model.addAttribute("item", accountSurveyService.getGraderConfigById(id));
+        return EDIT_VIEW;
+    }
+    
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.GET)
+    public String delete(@PathVariable Long id, Model model){
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+    	accountSurveyService.delete(accountSurveyService.getGraderConfigById(id));
+    	model.addAttribute("items",accountSurveyService.getAllGraderConfigs());
+        return LIST_VIEW;
+    }
+    
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public String view(@PathVariable Long id, Model model){
+    	model.addAttribute("model", MODEL);
+    	model.addAttribute("modelDisplay", MODEL_DISPLAY);
+        model.addAttribute("item", accountSurveyService.getGraderConfigById(id));
+        return DISPLAY_VIEW;
+    }
+     
+    @ModelAttribute("fieldnames")
+    public Field[] getFieldNames() {  	
+        return MODEL_CLASS.getDeclaredFields();
+    }
+}
