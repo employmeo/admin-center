9f85a332ee1a35e050dff920960684394983451c
Sri <sridharkaza@yahoo.com>
2021-01-01 15:58:01 -0800

    Update stop.sh

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-upload_dir=/home/ec2-user/drops
+upload_dir=/opt/talytica/application/install
 upload_jar=$(ls -rt1 ${upload_dir}/*.jar | tail -n1)
 RUNNING_PROCESS=$(ps -ef | grep 'sudo nohup java -jar ' | grep -v grep | awk '{print $2}')
 
 if [ "${upload_jar}" = null ]; then
-	echo "no files in upload dir"
-    else
+    echo "no files in upload dir"
+else
     echo "Copying ${upload_dir}/${upload_jar} to builds"
-	sudo cp ${upload_dir}/${upload_jar} ${build_dir}/	
-	sudo rm ${upload_dir}/*.jar
+    sudo cp ${upload_dir}/${upload_jar} ${build_dir}/	
+    sudo rm ${upload_dir}/*.jar
 fi
 
 if [ "${RUNNING_PROCESS:-null}" = null ]; then
@@ -16,7 +16,6 @@ if [ "${RUNNING_PROCESS:-null}" = null ]; then
 else
     echo "Currently running server processes:"
     echo $RUNNING_PROCESS
-
     sudo kill $RUNNING_PROCESS
     tail --pid=$RUNNING_PROCESS -f /dev/null
     echo "Server stop complete"
