0e34707eebea76852d6cda63d917024a80fc659d
Sri <sridharkaza@yahoo.com>
2021-01-01 15:50:09 -0800

    Update deploy-latest.sh

--- deploy/deploy-latest.sh
+++ deploy/deploy-latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-upload_dir=/home/ec2-user/drops
+upload_dir=/opt/talytica/application/install
 build_dir=/opt/talytica/application/builds
 deploy_dir=/opt/talytica/application/deploy
 
@@ -10,5 +10,4 @@ release_build=$(ls -rt1 ${build_dir}/*.jar | tail -n1)
 application_name=$(echo $release_build | awk -F'-' '{print $1}')
 
 rm -f ${deploy_dir}/*.jar
-
 ln -s ${release_build} ${deploy_dir}/${application_name}.jar
